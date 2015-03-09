9d8e587917e9abd0ac25b1718234445b3f10499a
jackyzhong123 <jackyzhong123@gmail.com>
2015-03-09 14:02:04 +0800

    Update MyPlugin.java

--- src/android/MyPlugin.java
+++ src/android/MyPlugin.java
@@ -1,1 +1,32 @@
 
+package cn.jackyzhong.myplugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyPlugin extends CordovaPlugin {
+	public static final String ACTION_ADD_CALENDAR_ENTRY = "sayHello"; 
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		try {
+			if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) { 
+		 
+			   callbackContext.success("Jacky");
+			   return true;
+			}
+			callbackContext.error("Invalid action");
+			return false;
+		} catch(Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			callbackContext.error(e.getMessage());
+			return false;
+		} 
+	}
+}
