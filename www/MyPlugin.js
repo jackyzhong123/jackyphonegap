fed8e24981f0da7ae0f6fcb5ad4eda43dbf577c6
jackyzhong123 <jackyzhong123@gmail.com>
2015-03-09 12:05:58 +0800

    Update MyPlugin.js

--- www/MyPlugin.js
+++ www/MyPlugin.js
@@ -1,23 +1,191 @@
-var exec = require('cordova/exec');
-/**
- * Constructor
- */
-function MyPlugin() {}
+cordova.define("de.websector.myplugin.MyPlugin", function (require, exports, module) {
+    var exec = require('cordova/exec');
+    /**
+     * Constructor
+     */
+    function MyPlugin() { };
 
-MyPlugin.prototype.sayHello = function() {
-  exec(function(result){
-      // result handler
-      alert(result);
+    MyPlugin.prototype.sayHello = function () {
+        exec(function (result) {
+            // result handler
+            alert(result);
+        },
+          function (error) {
+              // error handler
+              alert("Error" + error);
+          },
+          "MyPlugin",
+          "sayHello",
+          []
+        );
+    };
+
+    MyPlugin.prototype.encodeByDES = function (arg1) {
+      //  console.log("encode")
+        var deferred = $.Deferred();
+        //console.log("encode2");
+        exec(function (result, prevalue) {
+            
+            deferred.resolve(result, prevalue);
+        },
+          function (error) {
+              // error handler
+              alert("Error" + error);
+          },
+          "MyPlugin",
+          "encodeByDES",
+          [arg1]
+        );
+        //console.log("endencode")
+        return deferred.promise();
+    };
+
+
+
+
+
+    MyPlugin.prototype.setValueToLocal = function (key, value) {
+        var deferred = $.Deferred();
+        exec(function (key,value) {
+            deferred.resolve(key, value);
+        },
+        function (error) {
+            // error handler
+            alert("Error" + error);
+        },
+         "MyPlugin",
+          "setValueToLocal",
+          [key, value]
+        );
+        return deferred.promise();
+    };
+
+    MyPlugin.prototype.getValueToLocal = function (key) {
+        var deferred = $.Deferred();
+        exec(function (key, value) {
+            deferred.resolve(key, value);
+        },
+        function (error) {
+            // error handler
+            alert("Error" + error);
+        },
+         "MyPlugin",
+          "getValueToLocal",
+          [key]
+        );
+        return deferred.promise();
+
+    };
+
+
+
+    MyPlugin.prototype.jumpToMainTab = function () {
+      //  var deferred = $.Deferred();
+        exec(function ( ) {
+          // deferred.resolve(result);
+        },
+        function (error) {
+            // error handler
+            alert("Error" + error);
+        },
+         "MyPlugin",
+          "jumpToMainTab",
+          []
+        );
+      //  return deferred.promise();
+    };
+
+    MyPlugin.prototype.jumpToXGCQ = function (arg1,arg2) {
+        //  var deferred = $.Deferred();
+        exec(function () {
+            // deferred.resolve(result);
+        },
+        function (error) {
+            // error handler
+            alert("Error" + error);
+        },
+         "MyPlugin",
+          "jumpToXGCQ",
+          [arg1, arg2]
+        );
+        //  return deferred.promise();
+    };
+
+    MyPlugin.prototype.jumpToBack = function () {
+        //  var deferred = $.Deferred();
+        exec(function () {
+            // deferred.resolve(result);
+        },
+        function (error) {
+            // error handler
+            alert("Error" + error);
+        },
+         "MyPlugin",
+          "jumpToBack",
+          []
+        );
+        //  return deferred.promise();
+    };
+
+    MyPlugin.prototype.clearAccount = function () {
+        var deferred = $.Deferred();
+        exec(function () {
+            deferred.resolve(result);
+        },
+        function (error) {
+            alert("Error" + error);
+        },
+         "MyPlugin",
+          "clearAccount",
+          []
+        );
+        return deferred.promise();
+    };
+
+    MyPlugin.prototype.jumpToURL = function (arg, isSys) {
+        exec(function () {
+            // deferred.resolve(result);
+        },
+      function (error) {
+          alert("Error" + error);
+      },
+       "MyPlugin",
+        "jumpToURL",
+        [arg, isSys]
+      );
+
+    };
+
+    MyPlugin.prototype.downloadFileAsync = function (remoteURL, filePath) {
+        exec(function () {
+            // deferred.resolve(result);
+        },
+      function (error) {
+          alert("Error" + error);
+      },
+       "MyPlugin",
+        "downloadFileAsync",
+        [remoteURL, filePath]
+      );
+
+    };
+    MyPlugin.prototype.computeCacheSize = function () {
+        var deferred = $.Deferred();
+        exec(function (result) {
+            deferred.resolve(result);
+        },
+    function (error) {
+        alert("Error" + error);
     },
-    function(error){
-      // error handler
-      alert("Error" + error);
-    }, 
-    "MyPlugin", 
-    "sayHello", 
-    []
-  );
-}
+     "MyPlugin",
+      "computeCacheSize",
+      []
+    );
+        return deferred.promise();
+    };
 
-var myPlugin = new MyPlugin();
-module.exports = myPlugin
+
+
+    var myPlugin = new MyPlugin();
+    module.exports = myPlugin;
+});
