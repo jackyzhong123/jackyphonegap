9bfa39cf829c2787943c5e02e8754ae6c0ab5053
jackyzhong123 <jackyzhong123@gmail.com>
2015-03-09 12:03:52 +0800

    Update MyPlugin.h

--- src/ios/MyPlugin.h
+++ src/ios/MyPlugin.h
@@ -4,4 +4,27 @@
 
 - (void)sayHello:(CDVInvokedUrlCommand*)command;
 
+-(void)checkKey:(CDVInvokedUrlCommand*)command;
+-(void)saveKey:(CDVInvokedUrlCommand*)command;
+-(void)getKey:(CDVInvokedUrlCommand*)command;
+-(void)encodeByDES:(CDVInvokedUrlCommand*)command;
+-(void)decodeByDES:(CDVInvokedUrlCommand*)command;
+
+
+-(void)setValueToLocal:(CDVInvokedUrlCommand*)command;
+-(void)getValueToLocal:(CDVInvokedUrlCommand*)command;
+
+-(void)jumpToMainTab:(CDVInvokedUrlCommand*)command;
+
+-(void)jumpToXGCQ:(CDVInvokedUrlCommand*)command;
+-(void)jumpToBack:(CDVInvokedUrlCommand*)command;
+
+-(void)clearAccount:(CDVInvokedUrlCommand*)command;
+-(void)jumpToURL:(CDVInvokedUrlCommand*)command;
+
+-(void)downloadFileAsync:(CDVInvokedUrlCommand*)command;
+
+-(void)computeFolderSize:(CDVInvokedUrlCommand*)command;
+
+
 @end
