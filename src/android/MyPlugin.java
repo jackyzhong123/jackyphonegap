868dfea5e0e27840bc7af8192f2a16c8589213fb
jackyzhong123 <jackyzhong123@gmail.com>
2015-03-31 18:42:54 +0800

    Update MyPlugin.java

--- src/android/MyPlugin.java
+++ src/android/MyPlugin.java
@@ -21,10 +21,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.pengyouqiang.pengyouqiang.InternalActivity;
+import com.pengyouqiang.pengyouqiang.InternalCordova;
 import com.pengyouqiang.pengyouqiang.MainActivity;
 import com.pengyouqiang.uti.AsyncDownload;
 import com.pengyouqiang.uti.DESUti;
+import com.pengyouqiang.uti.DeleteFileUtil;
 
 import android.R.string;
 import android.app.Activity;
@@ -32,15 +33,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.net.rtp.RtpStream;
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 import com.pengyouqiang.pengyouqiang.*;
 import android.app.Activity;
 
 public class MyPlugin extends CordovaPlugin {
 	public static final String ACTION_ADD_CALENDAR_ENTRY = "sayHello";
-	 
 
 	@Override
 	public boolean execute(String action, JSONArray args,
@@ -61,8 +66,8 @@ public class MyPlugin extends CordovaPlugin {
 					resultString = "0";
 				}
 				JSONArray jsonStrs = new JSONArray();
-				jsonStrs.put(0,  args.getString(0));
-				jsonStrs.put(1,resultString);
+				jsonStrs.put(0, args.getString(0));
+				jsonStrs.put(1, resultString);
 				callbackContext.success(jsonStrs);
 				return true;
 			case "jumpToMainTab":
@@ -70,8 +75,16 @@ public class MyPlugin extends CordovaPlugin {
 						TabsActivity.class);
 				cordova.getActivity().startActivity(intent);
 				cordova.getActivity().finish();
-				return true;
-
+				return true;				
+			case "jumpToInternalCordova":
+				jumpToInternalCordova(args, callbackContext);
+				callbackContext.success();
+				return true;	 
+			case "jumpToInternalCordovaWithDetail":
+				jumpToInternalCordovaWithDetail(args, callbackContext);
+				callbackContext.success();
+				return true;	
+		 			
 			case "setValueToLocal":
 				setValueToLocal(args, callbackContext);
 				return true;
@@ -79,19 +92,50 @@ public class MyPlugin extends CordovaPlugin {
 				getValueToLocal(args, callbackContext);
 				return true;
 			case "checkCacheExisit":
-				SharedPreferences spPreferences = cordova.getActivity().getSharedPreferences(
-						"user", Context.MODE_PRIVATE);
-				 
-				checkCacheExisit(args,spPreferences.getString("PATH_NoCloud", ""), callbackContext);
+				SharedPreferences spPreferences = cordova.getActivity()
+						.getSharedPreferences("user", Context.MODE_PRIVATE);
+				checkCacheExisit(args,
+						spPreferences.getString("PATH_NoCloud", ""),
+						callbackContext);
 				return true;
-			case "JumpToView":
-				JumpToView(args, callbackContext);
+			
+			case "jumpToBack":
+				if(args.getBoolean(0))
+				{
+					cordova.getActivity().setResult(0);
+				} 
+				cordova.getActivity().finish();
 				callbackContext.success();
 				return true;
-			case "jumpToBack":
-				cordova.getActivity().finish();
+		 
+			case "jumpToURL":
+				jumpToURL(args, callbackContext);
+				callbackContext.success();
 				return true;
+			case "clearAccount":
+				clearAccount(args, callbackContext);
+				callbackContext.success();
+				return true;
+			case "computeCacheSize":
+				double size= computeCacheSize();
+			//	callbackContext.success();
+				  callbackContext.success(String.valueOf(size));
+				return true;
+			case "showMenuTabs":
+				showMenuTabs(args, callbackContext);
+				return true;	
+				case "injectDetailWebView":
+					injectDetailWebView(args, callbackContext);
+				return true;
+			case "clearimagecache":
+			 	String folderString= cordova.getActivity().getApplicationContext().getFilesDir().getAbsolutePath()+"/NoCloud"+"/imagecache";
+			//	 DeleteFileUtil.doDeleteEmptyDir(folderString);        
+			  
+				  DeleteFileUtil.DeleteFile(folderString); 
+				 double size1= computeCacheSize(); 
+				 callbackContext.success(String.valueOf(size1));
 				
+				return true;
 			default:
 				break;
 			}
@@ -105,38 +149,135 @@ public class MyPlugin extends CordovaPlugin {
 		}
 	}
 	
-	private void JumpToView(JSONArray args, CallbackContext callbackContext) throws JSONException
+	private void showMenuTabs(JSONArray args, CallbackContext callbackContext) throws JSONException
 	{
-		 String isInternal = args.getString(0);
-	     String url = args.getString(1);
-	     String page = args.getString(2);
-	     String data =  args.getString(3);
-	     if(isInternal.equals("internal"))
-	     {
-	    	
-				SharedPreferences preferences = cordova.getActivity().getSharedPreferences("user",
-						Context.MODE_PRIVATE);
-				Editor editor = preferences.edit();
-
-				editor.putString("JumpUrl", url);
-				editor.putString("JumpPage",page);
-				editor.putString("JumpData", data);
-				editor.commit();
-				 Intent intent = new Intent(cordova.getActivity(),
-							InternalActivity.class);
-					cordova.getActivity().startActivity(intent) ;
-			 
-			//		cordova.getActivity().finish();
-				
-	     }else {
-			
-		}
-	     
-	     
-	     
+		TabsActivity t= (TabsActivity) cordova.getActivity().getParent();				 
+		Message message = new Message();
+		Bundle bundle=new Bundle();
+		Boolean showTabs = args.getBoolean(0); 
+		bundle.putBoolean("show", showTabs);
+		message.setData(bundle);
+		//写逻辑
+		t.showHandler.sendMessage(message);
+	
+		
+		callbackContext.success();
+	}
+	
+	private void injectDetailWebView(JSONArray args, CallbackContext callbackContext) throws JSONException
+	{
+		InternalCordovaWithDetail t= (InternalCordovaWithDetail) cordova.getActivity();				 
+		Message message = new Message();
+		Bundle bundle=new Bundle();
+		String  data = args.getString(0); 
+		bundle.putString("data", data);
+		message.setData(bundle);
+		//写逻辑
+		t.showHandler.sendMessage(message);		
+		callbackContext.success();
+	}
+	
+	private double computeCacheSize()
+	{
+		
+		double filesize=0;
+		File file=new File(cordova.getActivity().getApplicationContext().getFilesDir().getAbsolutePath()+"/NoCloud"+"/imagecache" );
+	 
+		 if (!file.isFile()) {  
+             //获取文件大小  
+             File[] fl = file.listFiles();  
+         
+             double ss = 0;  
+             for (File f : fl)  
+                 ss +=  f.length()/1024/1024;
+             
+             filesize=ss;
+            // callbackContext.success(String.valueOf(ss));
+		 }
+		 return filesize;
+		// callbackContext.success(0);
+         
 	}
 	
 	
+ 
+	
+
+	private void jumpToInternalCordova(JSONArray args, CallbackContext callbackContext)
+			throws JSONException { 
+		String url = args.getString(0); 
+		String data = args.getString(1);
+		boolean needResult=args.getBoolean(2);
+			SharedPreferences preferences = cordova.getActivity()
+					.getSharedPreferences("user", Context.MODE_PRIVATE);
+			Editor editor = preferences.edit();
+			editor.putString("JumpUrl", url); 
+			editor.putString("JumpData", data);
+			editor.commit();
+			Intent intent = new Intent(cordova.getActivity(),
+					InternalCordova.class);
+			if(needResult)
+			{
+				cordova.getActivity().startActivityForResult(intent, 0); 
+			}else {				
+				cordova.getActivity().startActivity(intent); 
+			} 
+	}
+	
+	
+	private void jumpToInternalCordovaWithDetail(JSONArray args, CallbackContext callbackContext)
+			throws JSONException { 
+		String url = args.getString(0); 
+		String data = args.getString(1);
+		boolean needResult=args.getBoolean(2);
+			SharedPreferences preferences = cordova.getActivity()
+					.getSharedPreferences("user", Context.MODE_PRIVATE);
+			Editor editor = preferences.edit();
+			editor.putString("JumpUrl", url); 
+			editor.putString("JumpData", data);
+			editor.commit();
+			Intent intent = new Intent(cordova.getActivity(),
+				 InternalCordovaWithDetail.class);
+			if(needResult)
+			{
+				cordova.getActivity().startActivityForResult(intent, 0); 
+			}else {				
+				cordova.getActivity().startActivity(intent); 
+			} 
+	}
+	
+	
+
+	private void jumpToURL(JSONArray args, CallbackContext callbackContext)
+			throws JSONException {
+		SharedPreferences preferences = cordova.getActivity()
+				.getSharedPreferences("user", Context.MODE_PRIVATE);
+		String url = args.getString(0);
+		Editor editor = preferences.edit();
+		editor.putString("JumpUrl", url);
+		editor.commit();
+		Intent intent = new Intent(cordova.getActivity(),
+				ExternalActivity.class);
+		cordova.getActivity().startActivity(intent);
+	}
+
+	private void clearAccount(JSONArray args, CallbackContext callbackContext)
+			throws JSONException {
+		SharedPreferences preferences = cordova.getActivity()
+				.getSharedPreferences("user", Context.MODE_PRIVATE);
+		// String url = args.getString(0);
+		Editor editor = preferences.edit();
+		// editor.putString("JumpUrl", url);
+		editor.remove("access_token");
+		editor.putString("JumpUrl", "firstLogin.html"); 
+		editor.putString("JumpData", "");
+
+		editor.commit();
+		Intent intent = new Intent(cordova.getActivity(),
+				InternalCordova.class);
+		cordova.getActivity().startActivity(intent);
+		cordova.getActivity().finish();
+	}
 
 	private void getValueToLocal(JSONArray args, CallbackContext callbackContext)
 			throws Exception {
@@ -144,51 +285,49 @@ public class MyPlugin extends CordovaPlugin {
 				.getSharedPreferences("user", Context.MODE_PRIVATE);
 
 		JSONArray JSONgetValueToLocal = new JSONArray();
-		JSONgetValueToLocal.put(0,
+		JSONgetValueToLocal.put(0, args.getString(0));
+		JSONgetValueToLocal.put(1,
 				SPgetValueToLocal.getString(args.getString(0), ""));
-		JSONgetValueToLocal.put(1, args.getString(0));
+
 		callbackContext.success(JSONgetValueToLocal);
 	}
 
-	private void checkCacheExisit(JSONArray args,String NoCloudPath,
+	private void checkCacheExisit(JSONArray args, String NoCloudPath,
 			CallbackContext callbackContext) throws Exception {
-		ArrayList<String>  existArr = new ArrayList<String> (); 
-		ArrayList<String>  nonExistArr = new ArrayList<String> (); 
-		 
-		for(int i=0;i<args.length();i++)
-		{
-			File file = new File(NoCloudPath+"/imagecache/"+ args.getString(i));
-			if(file.exists())
-			{
+		ArrayList<String> existArr = new ArrayList<String>();
+		ArrayList<String> nonExistArr = new ArrayList<String>();
+
+		for (int i = 0; i < args.length(); i++) {
+			File file = new File(NoCloudPath + "/imagecache/"
+					+ args.getString(i));
+			if (file.exists()) {
 				existArr.add(args.getString(i));
-			}else {
-				
+			} else {
+
 				nonExistArr.add(args.getString(i));
 			}
 		}
 		JSONObject result = new JSONObject();
-		JSONArray jsonExist = new JSONArray();  
+		JSONArray jsonExist = new JSONArray();
 		JSONArray jsonNoExist = new JSONArray();
-		Iterator<String> it=existArr.iterator();
-		while(it.hasNext()){
-			jsonExist.put(it.next());			
+		Iterator<String> it = existArr.iterator();
+		while (it.hasNext()) {
+			jsonExist.put(it.next());
 		}
-		it=nonExistArr.iterator();
-		while(it.hasNext()){
-			jsonNoExist.put(it.next());			
+		it = nonExistArr.iterator();
+		while (it.hasNext()) {
+			jsonNoExist.put(it.next());
 		}
 		result.put("exsist", jsonExist);
 		result.put("nonexsist", jsonNoExist);
 		callbackContext.success(result);
-		//downloadFileAsync(nonExistArr);
-		AsyncDownload download=new AsyncDownload();
-		download.execute(nonExistArr,NoCloudPath+"/imagecache/");
+		// downloadFileAsync(nonExistArr);
+		AsyncDownload download = new AsyncDownload();
+		download.execute(nonExistArr, NoCloudPath + "/imagecache/");
 		return;
-	 
- 
+
 	}
-  
- 
+
 	private void setValueToLocal(JSONArray args, CallbackContext callbackContext)
 			throws Exception {
 		SharedPreferences SPsetValueToLocal = cordova.getActivity()
